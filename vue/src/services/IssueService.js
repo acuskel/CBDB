f79d9952cd8533826b6315f0bf16faef99b8d18a
Amanda Cuskelly <acuske@gmail.com>
2021-04-08 15:22:25 -0400

    update

--- vue/src/services/IssueService.js
+++ vue/src/services/IssueService.js
@@ -8,7 +8,7 @@ export default {
         return axios.get(path + '/' + collectionId)
     },
 
-    addIssue(issue, collectionId){
+    addIssue(issue){
         return axios.post(path + '/' + collectionId, issue)
     }
 }
