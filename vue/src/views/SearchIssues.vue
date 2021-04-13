18253cb2b325257ca677f454b14d003347ade053
RJ Melena <rmelena10@gmail.com>
2021-04-13 19:52:56 -0400

    changed IssueDisplay to search through list of .state.allIssues instead of issues in the collection selected

--- vue/src/views/SearchIssues.vue
+++ vue/src/views/SearchIssues.vue
@@ -1,37 +1,43 @@
 <template>
   <div>
-      <h1>Search</h1>
-      <b-table-simple striped hover>
-          <b-thead>
-              <b-tr>
-                  <b-th>Series Title</b-th>
-                  <b-th>Publication Date</b-th>
-              </b-tr>
-              <b-tr v-for="issue in allIssues" v-bind:key="issue.issueId">
-                  <b-td>{{issue.seriesTitle}}</b-td>
-                  <b-td>{{issue.releaseDate}}</b-td>
-                  </b-tr>
-          </b-thead>
-      </b-table-simple>
+    <h1>Search</h1>
+    <b-table-simple striped hover>
+      <b-thead>
+        <b-tr>
+          <b-th>Series Title</b-th>
+          <b-th>Publication Date</b-th>
+        </b-tr>
+        <b-tr v-for="issue in allIssues" v-bind:key="issue.issueId">
+          <b-td>
+            <b-link
+               :to="{
+                name: 'issue-display',
+                params: { id: issue.issueId },
+              }"
+            >
+              {{ issue.seriesTitle }}
+            </b-link>
+          </b-td>
+          <b-td>{{ issue.releaseDate }}</b-td>
+        </b-tr>
+      </b-thead>
+    </b-table-simple>
   </div>
 </template>
 
 <script>
 export default {
-    name: "search-issues",
-    data(){
-        return{
-            allIssues: []
-        }
-    },
-    created(){
-        this.allIssues = this.$store.state.allIssues;
-        console.log(this.allIssues.length);
-        console.log(this.allIssues)
-    }
-}
+  name: "search-issues",
+  data() {
+    return {
+      allIssues: [],
+    };
+  },
+  created() {
+    this.allIssues = this.$store.state.allIssues;
+  },
+};
 </script>
 
 <style>
-
 </style>
