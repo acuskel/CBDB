7aedefc6fb9b480dfcf60e4ba66b404faa5f1c08
Amanda Cuskelly <acuske@gmail.com>
2021-04-13 12:36:00 -0400

    added bootstrap

--- dotnet/Capstone/Models/Collection.cs
+++ dotnet/Capstone/Models/Collection.cs
@@ -9,7 +9,6 @@ namespace Capstone.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        
         public List<Issue> Issues { get; set; }
         public int UserId { get; set; }
         public bool IsPublic { get; set; }
--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,13 +1,62 @@
 <template>
   <div id="app">
-    <img src="../src/assets/logo.png"/>
+    <div id="bootstrap-navbar">
+      <b-navbar toggleable="lg" type="dark" variant="success">
+        <b-container>
+          <b-navbar-brand href="#">Mealzers</b-navbar-brand>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-collapse id="nav-collapse" is-nav>
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-form>
+                <b-form-input
+                  size="sm"
+                  class="mr-sm-2"
+                  placeholder="Search for a meal"
+                  v-model="meal"
+                ></b-form-input>
+                <b-button
+                  size="sm"
+                  class="my-2 my-sm-0"
+                  type="submit"
+                  @click.prevent="getMeal"
+                  >Search</b-button
+                >
+              </b-nav-form>
+              <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template slot="button-content"><em>User</em></template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-container>
+      </b-navbar>
+    </div>
+    <img src="../src/assets/logo.png" />
+
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'public' }">Browse Public Collections</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'register' }" v-if="$store.state.token == ''">Register</router-link>&nbsp;|&nbsp;
-       <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''"
+        >Home</router-link
+      >&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'public' }"
+        >Browse Public Collections</router-link
+      >&nbsp;|&nbsp;
+      <router-link
+        v-bind:to="{ name: 'register' }"
+        v-if="$store.state.token == ''"
+        >Register</router-link
+      >&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''"
+        >Login</router-link
+      >&nbsp;|&nbsp;
       <!-- <router-link v-bind:to="{ name: 'signup' }" v-if="$store.state.token != ''">Become a Premium User</router-link>&nbsp;|&nbsp;-->
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
     </div>
     <router-view />
   </div>
@@ -21,7 +70,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color:rgb(232,61, 79)
+  background-color: rgb(232, 61, 79);
 }
 
 #nav {
@@ -47,13 +96,13 @@ ul {
 
 #buttons li a {
   text-decoration: none;
-  color: #FFFFFF;
+  color: #ffffff;
   display: block;
 }
 
 li a:hover {
   text-decoration: none;
   color: #000000;
-  background-color: #33B5E5;
+  background-color: #33b5e5;
 }
 </style>
