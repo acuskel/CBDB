0c9fbbe9fe438d4401d7f308afc1b347be5cf241
RJ Melena <rmelena10@gmail.com>
2021-04-13 09:32:50 -0400

    pull 4-13, uncommented section in CollectionsList

--- vue/src/components/CollectionsList.vue
+++ vue/src/components/CollectionsList.vue
@@ -26,11 +26,11 @@ export default {
       message: "",
     };
   },
-  computed: {
-    collections() {
-      return this.$store.state.collections;
-    }
-  },
+ // computed: {
+   // collections() {
+   //   return this.$store.state.collections;
+   // }
+//  },
   created(){
     this.message = "";
 
