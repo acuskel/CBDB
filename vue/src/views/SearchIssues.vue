169447ef22bc43a0deb81eaec327bd4025e80582
RJ Melena <rmelena10@gmail.com>
2021-04-13 17:35:49 -0400

    SearchIssue table displaying seriesName and releaseDate

--- vue/src/views/SearchIssues.vue
+++ vue/src/views/SearchIssues.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-      <b-table v-bind:key="issue.issueId" v-for="issue in allIssues"></b-table>
+      <h1>Search</h1>
+      <b-table-simple striped hover>
+          <b-thead>
+              <b-tr>
+                  <b-th>Series Title</b-th>
+                  <b-th>Publication Date</b-th>
+              </b-tr>
+              <b-tr v-for="issue in allIssues" v-bind:key="issue.issueId">
+                  <b-td>{{issue.seriesTitle}}</b-td>
+                  <b-td>{{issue.releaseDate}}</b-td>
+                  </b-tr>
+          </b-thead>
+      </b-table-simple>
   </div>
 </template>
 
