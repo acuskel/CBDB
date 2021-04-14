88291bf0f0b56125496df9f081b476d22aed1a96
Amanda Cuskelly <acuske@gmail.com>
2021-04-14 16:28:51 -0400

    Issue CSS

--- vue/src/components/IssueInfo.vue
+++ vue/src/components/IssueInfo.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
-    <div id="issue-data">
-      <h1>{{ issue.title }}</h1>
-      <h3>Title: {{ issue.seriesTitle }}</h3>
-      <h3>Creator: {{ issue.creator }}</h3>
-      <h3>Publisher: {{ issue.publisher }}</h3>
-      <h3>Characters: {{ issue.characters }}</h3>
-      <h3>Summary: {{ issue.summary }}</h3>
-      <h3>Publication Date: {{ issue.releaseDate }}</h3>
-      <h3>Page Count: {{ issue.pageCount }}</h3>
-    </div>
-        <h1>Add to Collection:</h1>
-        <b-button 
+    <b-container id="issue-data">
+      
+      <b-row>
+      <b-col class="text-left">
+        <h1 class="title">{{ issue.seriesTitle }}</h1>
+      <p class="data"><b>Creator:</b> {{ issue.creator }}</p>
+      <p class="data"><b>Publisher:</b> {{ issue.publisher }}</p>
+      <p class="data"><b>Publication Date:</b> {{ issue.releaseDate }}</p>
+      <p class="data"><b>Page Count:</b> {{ issue.pageCount }}</p>
+      <p><b>Featuring: </b>{{ issue.characters }}</p><br>
+      <h3>Add to collection:</h3>
+      <b-container class="addToCollection" >
+      <b-row>
+        <b-col ><b-button 
         variant="primary"
         v-for="unaddedCollection in unaddedCollections"
         v-bind:key="unaddedCollection.id" 
-        v-on:click="addToCollection(issue, unaddedCollection.id)">{{ unaddedCollection.name }}</b-button>
+        v-on:click="addToCollection(issue, unaddedCollection.id)" v-b-tooltip.hover="'Click to add comic to collection!'"
+        >{{ unaddedCollection.name }}</b-button></b-col>
+        </b-row>
+    </b-container>
+      </b-col>
+      <b-col class="text-right">
+      <img v-if="issue.publisher == 'Dark Horse'" src="https://d2lzb5v10mb0lj.cloudfront.net/darkhorse/index_images/blog/blog2015/sdcc15logo.jpg">
+      <img v-if="issue.publisher == 'Marvel'" width=300 src="https://logodownload.org/wp-content/uploads/2017/05/marvel-logo-0.png">
+      <img v-if="issue.publisher == 'DC'" width=300 src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/600px-DC_Comics_logo.svg.png">
+      <img v-if="issue.publisher == 'Centaur'" src="https://comicvine1.cbsistatic.com/uploads/scale_small/1/19772/619139-centaur.gif">
+      </b-col>
+      </b-row>
+      </b-container>
+    <div class="summary">  
+    <h1>Synopsis</h1><br>
+    <p>{{ issue.summary }}</p>
+    </div>
     <div>
       <!-- <b-form-rating v-model="value" variant="warning" class="mb-3"></b-form-rating> -->
     </div>
@@ -95,10 +113,24 @@ export default {
 
 <style scoped>
 div {
-  background-color: white;
+  color:white;
 }
 button{
-  margin:5px;
+  margin-left:-15px;
+  margin-right: 30px;
+  margin-top: 10px;
 }
 
+.summary {
+  margin-top:80px;
+  margin-bottom:80px;
+}
+
+.data {
+  font-size: 18px;
+}
+
+.summary {
+  font-size: 20px;
+}
 </style>
