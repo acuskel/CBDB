4649b6f6a5cac3bac04f8ca5be59ecc8c5eb653d
Amanda Cuskelly <acuske@gmail.com>
2021-04-14 11:01:44 -0400

    fixed foot.vue

--- vue/src/components/Foot.vue
+++ vue/src/components/Foot.vue
@@ -3,37 +3,11 @@
     <share-it
       text="This is sample text"
       url="https://www.google.com"
-      height="600"
-      width="600"
+      height= 600
+      width= 600
       dense
     />
-    <base-button class="btn-facebook btn-icon-only">
-      <span class="btn-inner--icon"><i class="fab fa-facebook"></i></span>
-    </base-button>
-    <base-button class="btn-twitter btn-icon-only">
-      <span class="btn-inner--icon"><i class="fab fa-twitter"></i></span>
-    </base-button>
-    <base-button class="btn-google-plus btn-icon-only">
-      <span class="btn-inner--icon"><i class="fab fa-google-plus-g"></i></span>
-    </base-button>
-    <base-button class="btn-instagram btn-icon-only">
-      <span class="btn-inner--icon"><i class="fab fa-instagram"></i></span>
-    </base-button>
-    <base-button class="btn-pinterest btn-icon-only">
-      <span class="btn-inner--icon"><i class="fab fa-pinterest"></i></span>
-    </base-button>
-    <base-button class="btn-youtube btn-icon-only">
-      <span class="btn-inner--icon"><i class="fab fa-youtube"></i></span>
-    </base-button>
-    <base-button class="btn-vimeo btn-icon-only">
-      <span class="btn-inner--icon"><i class="fab fa-vimeo-v"></i></span>
-    </base-button>
-    <base-button class="btn-slack btn-icon-only">
-      <span class="btn-inner--icon"><i class="fab fa-slack"></i></span>
-    </base-button>
-    <base-button class="btn-dribbble btn-icon-only">
-      <span class="btn-inner--icon"><i class="fab fa-dribbble"></i></span>
-    </base-button>
+    
   </div>
 </template>
 
