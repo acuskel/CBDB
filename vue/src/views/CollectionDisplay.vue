e72fac21613f7aa57e1b7a1f8018c58d250397b4
Josh Wood <joshwood@joshkwood.com>
2021-04-14 20:39:18 -0400

    Updated capstone script to include Issue Numbers. Page Count and Series ID are now INTs

--- vue/src/views/CollectionDisplay.vue
+++ vue/src/views/CollectionDisplay.vue
@@ -16,17 +16,9 @@ export default {
   },
 };
 </script>
-<<<<<<< HEAD
-<style>
-html, 
-body {
-    background-color: lightgray;
-}
-=======
 
 <style>
 .issue-list{
   background-color:rgba(26, 24, 24, 0.966);
   }
->>>>>>> 88c473f5d3019d6fe57bd7cb8bce43bdede26065
 </style>
